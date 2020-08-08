2496672e58433b7b8af3e89bf1ea9b50438ccffa
unknown <thaivanphu.qn95@gmail.com>
2020-08-08 21:41:40 +0700

    Data Type: List

---
+++ ex-list.py
@@ -0,0 +1,16 @@
+# An Introduction to Lists
+
+topics = ["Python", "Java", "AI", "R"]
+print(topics)
+print(type(topics))
+
+topics_2 = ["VueJS", "PHP", "Angular"]
+print(topics_2)
+
+print(topics[1])
+print(topics_2[0])
+
+print(type(topics[0]))
+
+print(topics[-1])
+print("My favorite area of study is " + topics[0].upper() + "!")
