b50212b5d9d0aafc323a3a2b1ff472137c63db9f
unknown <thaivanphu.qn95@gmail.com>
2020-08-15 16:08:09 +0700

    Tuples

---
+++ ex-tuples.py
@@ -0,0 +1,16 @@
+# Tuples
+
+array_a = [100, 92, 65]
+scores = (88, 92, 77)
+
+print(array_a)
+print(scores)
+
+print(type(array_a))
+print(type(scores))
+
+array_a.append(72)
+print(array_a)
+
+scores = (72, 89, 91)
+print(scores)
